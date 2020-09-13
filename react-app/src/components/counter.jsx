65e4055380e7be88727f985e0e00b2cd70d68b09
Gayathri Buddhika <wg.gayathri@gmail.com>
2020-09-13 23:12:08 +0530

    test some basis of react JS

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -2,17 +2,29 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = { 
-        name: 'Gayathri Buddhika'
+        name: 'Gayathri Buddhika',
+        count: 0,
+        imgUrl: 'https://picsum.photos/200' 
      }
     render() { 
         return ( 
-            <React.Fragment>
+            <div>
                 <h1>Hello World</h1> 
-                <button>Increment</button>
-                <h3>{this.state.name}</h3>
-            </React.Fragment>
+                <button className = "btn btn-success">Increment</button>
+                <h3>{ this.state.name }</h3>
+                <h3>{ this.formatCount() }</h3>
+                <img src={this.state.imgUrl}/>
+            </div>
         
         );
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
+        // const a = <h1>Zero</h1>;
+        // return count === 0 ? a : count;
+        // or return count === 0 ? <h1>Zero</h1> : count; 
     }
 }
  
