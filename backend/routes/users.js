7ed50dd9584912946b1d8c289ce51d000aab14dc
Gayathri Buddhika <wg.gayathri@gmail.com>
2021-01-07 17:53:27 +0530

    add update function

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,7 +3,7 @@ let User = require('../models/user.model');
 
 router.route('/').get((req, res) => {
     User.find()
-    .then(users => res.json(users))
+    .then(user => res.json(user))
     .catch(err => res.status(400).json('Error' + err));
 });
 
