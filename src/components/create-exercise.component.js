0e689839a7f65f6a63b1a7501f52e03dec3aaee1
Gayathri Buddhika <wg.gayathri@gmail.com>
2021-01-09 14:02:10 +0530

    created need all the components

---
+++ src/components/create-exercise.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export default class CreateExercise extends Component {
+    render () {
+        return (
+            <div>
+                <h3>This is CreateExercise Component</h3>
+            </div>
+        );
+    }
+}
