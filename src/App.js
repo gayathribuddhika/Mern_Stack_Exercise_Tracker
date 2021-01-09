650fefc9717c5cdc74f41518ffeb69e787763617
Gayathri Buddhika <wg.gayathri@gmail.com>
2021-01-09 13:48:18 +0530

    added navbar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router} from "react-router-dom";
 
-// import Navbar from "./components/navbar.component";
+import Navbar from "./components/navbar.component";
 // import ExerciseList from "./components/exercise-list.component";
 // import EditExercise from "./components/edit-exercise.component";
 // import CreateExercise from "./components/create-exercise.component";
@@ -11,14 +12,14 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <h1>hello</h1>
-    // <Router>
-    //   <Navbar />
-    //   <Route path = "/" exact component={ExerciseList}/>
-    //   <Route path = "/edit/:id" component={EditExercise}/>
-    //   <Route path = "/create" component={CreateExercise}/>
-    //   <Route path = "/user" component={CreateUser}/>
-    // </Router>
+    // <h1>hello</h1>
+    <Router>
+      <Navbar />
+      {/* <Route path = "/" exact component={ExerciseList}/>
+      <Route path = "/edit/:id" component={EditExercise}/>
+      <Route path = "/create" component={CreateExercise}/>
+      <Route path = "/user" component={CreateUser}/> */}
+    </Router>
   );
 }
 
