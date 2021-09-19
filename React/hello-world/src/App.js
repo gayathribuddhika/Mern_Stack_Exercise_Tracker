86c61a04598daf9fa6bd94412f310074ed58bf0b
Gayathri Buddhika <39160904+gayathribuddhika@users.noreply.github.com>
2021-09-20 00:04:40 +0530

    create setState method

--- React/hello-world/src/App.js
+++ React/hello-world/src/App.js
@@ -1,13 +1,15 @@
 // import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
-import Hello from './components/Hello';
+// import Greet from './components/Greet';
+// import Welcome from './components/Welcome';
+// import Hello from './components/Hello';
+import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Gayathri" mothername="Wasanthi"> 
+      <Message/>
+      {/* <Greet name="Gayathri" mothername="Wasanthi"> 
         <p>This is a children prop</p>
       </Greet>
       <Greet name="Tharushi" mothername="Wasanthi">
@@ -15,7 +17,7 @@ function App() {
       </Greet>
       <Greet name="Sanduni" mothername="Kumari"/>
       <Welcome name="Gayathri"/>
-      <Hello/>
+      <Hello/> */}
     </div>
   );
 }
