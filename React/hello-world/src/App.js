cacbfdd14ca41aadae6c992dad12561a0a302d65
Gayathri Buddhika <39160904+gayathribuddhika@users.noreply.github.com>
2021-09-20 23:32:12 +0530

    create a file counter.js

--- React/hello-world/src/App.js
+++ React/hello-world/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 // import Greet from './components/Greet';
 // import Welcome from './components/Welcome';
 // import Hello from './components/Hello';
-import Message from './components/Message';
+// import Message from './components/Message';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
-      <Message/>
+      <Counter />
+      {/* <Message/> */}
       {/* <Greet name="Gayathri" mothername="Wasanthi"> 
         <p>This is a children prop</p>
       </Greet>
---
+++ React/hello-world/src/components/Counter.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+    render (){
+        return (
+            <div>
+                <h1>Count</h1>
+            </div>
+        )
+    }
+}
+
+
+export default Counter;
