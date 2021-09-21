288ce2fef9debd072f5b546950bb318e8ad9a3e9
Gayathri Buddhika <39160904+gayathribuddhika@users.noreply.github.com>
2021-09-21 11:02:47 +0530

    setState method

--- React/hello-world/src/components/Counter.js
+++ React/hello-world/src/components/Counter.js
@@ -1,14 +1,31 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-    render (){
-        return (
-            <div>
-                <h1>Count</h1>
-            </div>
-        )
-    }
-}
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increment() {
+    // this.state.count = this.state.count + 1
+    // console.log(this.state.count)
+    this.setState(
+        { count: this.state.count + 1 },() => console.log("callback value ", this.state.count)
+    )
+    // console.log(this.state.count);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Count = {this.state.count}</h1>
+        <button onClick={() => this.increment()}>Increment</button>
+      </div>
+    );
+  }
+}
 
 export default Counter;
